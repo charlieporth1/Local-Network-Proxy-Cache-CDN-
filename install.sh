a4632b7ce12b4a4cab9e22c5197bbc527724a2f7
CHarles Porth <charlieporth1@gmail.com>
2021-02-17 11:07:22 -0600

    Minor changes to dns

--- install.sh
+++ install.sh
@@ -5,14 +5,16 @@ CONFIG_DIR=$CURRENT_DIR/config
 INSTALL_CONFIG_DIR=/etc/
 INSTALL_BIN_DIR=/usr/local/bin
 
+IPS=`ifconfig | grep "en\|wlan0" -A 1 | grep -oE 'inet ([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})' | grep -oE '([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})'`
 apt -y install unbound squid
+
+echo "dns_nameservers $IPS" | sudo tee -a $CONFIG_DIR/squid/debian.conf
 
 cp -rf $CONFIG_DIR/squid/* $INSTALL_CONFIG_DIR/squid/conf.d/
 cp -rf $CONFIG_DIR/unbound/* $INSTALL_CONFIG_DIR/unbound/unbound.conf.d/
 
 
 bash $CURRENT_DIR/apply.sh
-IPS=`ifconfig | grep "en\|wlan0" -A 1 | grep -E '([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})' | grep -oE 'inet ([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})'`
 echo """
 Point your devices to the http(s), ftp, socks, rstp proxy of $IPS and your DNS servers of your router and devices to $IPS
 Have a great speedy day

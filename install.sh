39f8df35df8992ea2b24eb7f08d257bdfa91dd20
CHarles Porth <charlieporth1@gmail.com>
2021-02-17 11:15:34 -0600

    minor updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function check_root () {
   if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root" 
+    echo "This script must be run as root"
     exit 1
   fi
 }
@@ -16,14 +16,15 @@ INSTALL_BIN_DIR=/usr/local/bin
 IPS=`ifconfig | grep "en\|wlan0" -A 1 | grep -oE 'inet ([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})' | grep -oE '([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})'`
 apt -y install unbound squid
 
-echo "dns_nameservers $IPS" | sudo tee -a $CONFIG_DIR/squid/debian.conf
 
 cp -rf $CONFIG_DIR/squid/* $INSTALL_CONFIG_DIR/squid/conf.d/
 cp -rf $CONFIG_DIR/unbound/* $INSTALL_CONFIG_DIR/unbound/unbound.conf.d/
 
+echo "dns_nameservers $IPS" | sudo tee -a $INSTALL_CONFIG_DIR/squid/conf.d/debian.conf
+
 
 bash $CURRENT_DIR/apply.sh
-echo """
-Point your devices to the http(s), ftp, socks, rstp proxy of $IPS and your DNS servers of your router and devices to $IPS
-Have a great speedy day
+printf """
+	Point your devices to the http(s), ftp, socks, rstp proxy of $IPS and your DNS servers of your router and devices to $IPS
+	Have a great speedy day
 """

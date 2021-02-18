3b0c01ea139ed556c91069861a6d313cf6f0372f
CHarles Porth <charlieporth1@gmail.com>
2021-02-18 09:30:30 -0600

    Added kernel improvements

--- kernelImprovements.sh
+++ kernelImprovements.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 KERNEL_CONF=/etc/sysctl.conf
+MOD_CONF=/etc/modules-load.d/ctp-cdn-cache.conf
+echo 'tcp_bbr' | sudo tee -a $MOD_CONF
+echo 'sctp' | sudo tee -a $MOD_CONF
+echo 'ip_tunnel' | sudo tee -a $MOD_CONF
+echo 'tcp_htcp' | sudo tee -a $MOD_CONF
+echo 'ipcomp' | sudo tee -a $MOD_CONF
+echo 'udp_tunnel' | sudo tee -a $MOD_CONF
+echo 'sha256_generic' | sudo tee -a $MOD_CONF
+echo 'ecdh_generic' | sudo tee -a $MOD_CONF
+echo 'gcm' | sudo tee -a $MOD_CONF
+echo 'xor' | sudo tee -a $MOD_CONF
+echo 'aes-neon-bs' | sudo tee -a $MOD_CONF
+echo 'aes-neon-blk' | sudo tee -a $MOD_CONF
+echo 'aes-arm64' | sudo tee -a $MOD_CONF
+echo 'xor-neon' | sudo tee -a $MOD_CONF
+echo 'poly1305-neon' | sudo tee -a $MOD_CONF
+echo 'chacha-neon' | sudo tee -a $MOD_CONF
+
 echo 'net.ipv4.tcp_timestamps=0' | sudo tee -a $KERNEL_CONF
 echo 'net.ipv4.tcp_sack=1' | sudo tee -a $KERNEL_CONF
 echo 'net.core.netdev_max_backlog=250000' | sudo tee -a $KERNEL_CONF
@@ -14,5 +32,4 @@ echo 'net.ipv4.tcp_low_latency=1' | sudo tee -a $KERNEL_CONF
 echo 'net.ipv4.tcp_adv_win_scale=1' | sudo tee -a $KERNEL_CONF
 echo 'net.core.default_qdisc=fq"' | sudo tee -a $KERNEL_CONF
 echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a $KERNEL_CONF
-#echo '' | sudo tee -a $KERNEL_CONF
 sudo sysctl -p

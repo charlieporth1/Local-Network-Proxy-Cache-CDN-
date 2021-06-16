0777f0b4ae389c090d308616d4b80b42e88b20ae
Charlie Porth <charlieporth1@gmail.com>
2021-06-16 18:56:14 -0500

    ipdate tto swapfile

--- createSwap.sh
+++ createSwap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # round $1 to $2 decimal places
+SWAP_FILE_SIZE_INPUT="${1}"
 round() {
     printf "%.${2:-0}f" "$1"
 }
@@ -12,10 +13,12 @@ echo $MEM_COUNT
 #If more than 12 GB of free space and less than 4GB of ram create swap if swapfile does not exist
 if [[ `echo $FREE_SPACE | rev | cut -c 2- | rev` -ge 12 ]] && [[ $MEM_COUNT -le 4096 ]] && ! [[ -f /swapfile ]]; then
 	sudo touch /swapfile
-	sudo dd if=/dev/zero of=/swapfile bs=512 count=$(( $MEM_COUNT * 8 ))K status=progress
+	[[ $MEM_COUNT -le 1024 ]] && MX=3 || MX=1
+	DD_MX=4
+	SWAP_FILE_SIZE=[[ -n "${SWAP_FILE_SIZE_INPUT}" ]] && echo $SWAP_FILE_SIZE_INPUT || echo $MEM_COUNT
+	sudo dd if=/dev/zero of=/swapfile bs=512 count=$(( $SWAP_FILE_SIZE * ( $DD_MX * 2  ) $MX ))K status=progress
 	sudo chmod 0600 /swapfile
-	[[ $MEM_COUNT -le 1024 ]] && MX=3 
-	sudo fallocate -l $(( $MEM_COUNT * ( 4 ) ))M /swapfile
+	sudo fallocate -l $(( $SWAP_FILE_SIZE * ( $MX ) * $MX ))M /swapfile
 	sudo mkswap /swapfile
 	sudo swapon /swapfile
 	sudo cp /etc/fstab /etc/fstab.bak
